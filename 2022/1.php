485f939a24137cc592c0889ee1279987ec7d11ba
Marten Tacoma <70998056+MartenTacoma@users.noreply.github.com>
2022-12-02 11:07:18 +0100

    Make 2022/1 more verbose

--- 2022/1.php
+++ 2022/1.php
@@ -1,5 +1,11 @@
 <?php
+$start = microtime(true);
+function printMsg($msg){
+	echo date('H:i:s').' | '.$msg . "\n";
+}
+printMsg('Start');
 $lines = file('1.txt');
+printMsg('File read done');
 $res = [0];
 $i = 0;
 foreach($lines as $line){
@@ -11,7 +17,10 @@ foreach($lines as $line){
 		$res[$i] = 0;
 	}
 }
+printMsg('Summed');
 rsort($res);
+printMsg('And now for the results');
 echo 'Max: ' . $res[0] . "\n";
 $part = array_slice($res, 0, 3);
 echo 'Sum top 3: ' . array_sum($part) . "\n";
+printMsg('Done in '.(microtime(true)-$start));
