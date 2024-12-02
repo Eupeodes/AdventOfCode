d3fff92f5a5ff665c738c2bf86fda2d8b2ec41ca
Marten Tacoma <8545271+Eupeodes@users.noreply.github.com>
2024-12-02 09:58:15 +0100

    Improve efficiency of dampener 2024 day 2

--- AdventOfCode/Y2024/D2.php
+++ AdventOfCode/Y2024/D2.php
@@ -6,21 +6,30 @@ use Eupeodes\AdventOfCode\Day;
 
 class D2 extends Day{
 	private $dataFile = __DIR__.'/D2.txt';
+	private $deltas = [];
 	public function __construct(){
 	}
 	
 	private function isSave($numbers){
-		$deltas = [];
-		for($n=1;$n<count($numbers);$n++){
-			$deltas[] = $numbers[$n] - $numbers[$n -1];
+		if(count($numbers) < 2){
+			return true;
 		}
-		if((min($deltas) >= 1 && max($deltas) <= 3) || (min($deltas) >= -3 && max($deltas) <= -1)){
+		$this->deltas = [];
+		for($n=1;$n<count($numbers);$n++){
+			$this->deltas[] = $numbers[$n] - $numbers[$n -1];
+		}
+		if((min($this->deltas) >= 1 && max($this->deltas) <= 3) || (min($this->deltas) >= -3 && max($this->deltas) <= -1)){
 			return true;
 		}
 		return false;
 	}
 	
 	private function dampener($numbers){
+		$dist = array_keys(array_count_values($this->deltas));
+		sort($dist);
+		if(max(array_slice($dist, 0, 2)) < -3 || min(array_slice($dist, -2)) > 3){
+			return false;
+		}
 		for($n=0;$n<count($numbers);$n++){
 			$part = array_values(array_diff_key($numbers, [$n=>'x']));
 			if($this->isSave($part)){
